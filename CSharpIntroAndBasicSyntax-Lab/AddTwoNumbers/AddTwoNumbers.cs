f94d95facf9f1b8e168215894fb16af903c1b667
ivanrk <ivanrk95@gmail.com>
2017-09-26 19:34:05 +0300

    First homework added

---
+++ CSharpIntroAndBasicSyntax-Lab/AddTwoNumbers/AddTwoNumbers.cs
@@ -0,0 +1,16 @@
+﻿namespace AddTwoNumbers
+{
+    using System;
+
+    public class AddTwoNumbers
+    {
+        public static void Main()
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var sum = firstNumber + secondNumber;
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+        }
+    }
+}
---
+++ CSharpIntroAndBasicSyntax-Lab/Greeting/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Greeting
+{
+    public class Greeting
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
