133db7bcccc79a078d8cba6d033b92f55380fefb
ivanrk <ivanrk95@gmail.com>
2017-09-30 12:50:52 +0300

    03. Conditional Statements and Loops added

---
+++ 03. CSharpConditionalStatementsAndLoops-Lab/MultiplicationTable2.0/MultiplicationTable.cs
@@ -0,0 +1,27 @@
+﻿namespace MultiplicationTable2_0
+{
+    using System;
+
+    public class MultiplicationTable
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{number} X {multiplier} = {number * multiplier}");
+            }
+
+            else
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine($"{number} X {i} = {number * i}");
+                }
+            }
+        }
+    }
+}
+
